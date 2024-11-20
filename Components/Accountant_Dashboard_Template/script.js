821b4e78b33d1406544fff63b547a4f4e247b179
ravindugamage168 <ravindugamage168@gmail.com>
2024-11-20 21:20:00 +0530

    Update script.js

--- Components/Accountant_Dashboard_Template/script.js
+++ Components/Accountant_Dashboard_Template/script.js
@@ -3,7 +3,7 @@ class Dashboard extends HTMLElement {
         this.innerHTML = `
             <section id="sidebar">
                 <a href="#" class="logo">
-                    <img src="../../Images/logo.png" width="90" height="90" alt="SAF GEMS">
+                    <img src="../../../Images/logo.png" width="90" height="90" alt="SAF GEMS">
                 </a>
                 <ul class="side-menu">
                     <li>
