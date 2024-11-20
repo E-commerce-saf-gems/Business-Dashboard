d3cfc16950afaced7ba3e2465bc6309b405a6606
Sadheeya <sadheeyasalim10@gmail.com>
2024-11-20 19:30:00 +0530

    Fixed Drop down problem in buyer select

--- Sales_Rep_Dashboard/Pages/Inventory/script.js
+++ Sales_Rep_Dashboard/Pages/Inventory/script.js
@@ -145,21 +145,3 @@ window.addEventListener('resize', function () {
 	}
 })
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    const buyerDropdown = document.getElementById('buyer');
-  
-    fetch('./getBuyer.php')
-        .then(response => response.json())
-        .then(buyers => {
-            buyers.forEach(buyer => {
-                const option = document.createElement('option');
-                option.value = buyer.buyer_id;
-                option.textContent = buyer.email;
-                buyerDropdown.appendChild(option);
-            });
-        })
-        .catch(error => {
-            console.error('Error fetching buyers:', error);
-        });
-  });
