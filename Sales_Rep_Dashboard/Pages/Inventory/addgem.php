71cd850a175946b887903f5d7c1a5e6c3cac04ba
ChethanaKavi <138358211+ChethanaKavi@users.noreply.github.com>
2025-02-28 20:18:15 +0530

    few changes

--- Sales_Rep_Dashboard/Pages/Inventory/addgem.php
+++ Sales_Rep_Dashboard/Pages/Inventory/addgem.php
@@ -43,8 +43,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             move_uploaded_file($_FILES["certificate"]["tmp_name"], $certificatePath)) {
 
             // Prepare SQL query for inventory table
-            $sql = "INSERT INTO inventory (shape, colour, type, size, origin, amount, image, certificate, description, visibility, availability, buyer_id)
-                    VALUES ('$shape', '$color', '$type', '$weight', '$origin', '$amount', '$image', '$certificate', '$description', '$visibility', '$availability','$buyer_id')";
+            $sql = "INSERT INTO inventory (shape, colour, type, size, origin, amount, image, certificate, description, visibility, buyer_id)
+                    VALUES ('$shape', '$color', '$type', '$weight', '$origin', '$amount', '$image', '$certificate', '$description', '$visibility','$buyer_id')";
 
             // Execute inventory query
             if ($conn->query($sql) === TRUE) {
