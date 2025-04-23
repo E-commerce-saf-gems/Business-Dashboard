3b979e9b51a22cd6d1d6a5c588e062cb3d644643
ChethanaKavi <138358211+ChethanaKavi@users.noreply.github.com>
2025-04-23 10:56:03 +0530

    partner dashboard customer navigation

---
+++ Partners_Dashboard/Pages/Customer/updatecustomer.php
@@ -0,0 +1,36 @@
+<?php
+include('../../../database/db.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve and sanitize POST data
+    $user_id = isset($_POST['user_id']) ? $_POST['user_id'] : null;
+    $username = isset($_POST['username']) ? $_POST['username'] : null;
+    $password = isset($_POST['password']) ? $_POST['password'] : null;
+    $name = isset($_POST['name']) ? $_POST['name'] : null;
+    $role = isset($_POST['role']) ? $_POST['role'] : null;
+    $email = isset($_POST['email']) ? $_POST['email'] : null;
+    $contactNo = isset($_POST['contactNo']) ? $_POST['contactNo'] : null;
+
+ 
+    // Prepare and execute the inventory update
+    $sql = "UPDATE customer SET role = ?, name = ?, email = ?, contactNo = ? WHERE user_id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sssii", $role, $name, $email, $contactNo, $user_id);
+    
+
+    if ($stmt->execute()) {
+       
+        echo "Record updated successfully.";
+        header("Location: ./Staff.php?editSuccess=1");
+        exit();
+    } else {
+        echo "Error updating record: " . $stmt->error;
+        header("Location: ./Staff.php?editSuccess=2");
+    }
+
+    $stmt->close();
+    $conn->close();
+} else {
+    echo "Invalid request method.";
+}
+?>
