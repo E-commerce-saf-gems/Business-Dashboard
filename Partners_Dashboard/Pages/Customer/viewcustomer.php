05529b23ef14b65930884d1ea50740a8c020da18
ChethanaKavi <138358211+ChethanaKavi@users.noreply.github.com>
2025-04-28 00:57:39 +0530

    customer css

--- Partners_Dashboard/Pages/Customer/viewcustomer.php
+++ Partners_Dashboard/Pages/Customer/viewcustomer.php
@@ -28,8 +28,9 @@ if (isset($_GET['id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Customer Details</title>
-    <link rel="stylesheet" href="../../../Components/Admin_Dashboard_Template/styles.css">
-    <link rel="stylesheet" href="../editCustomerStyles.css">   
+    <link rel="stylesheet" href="../../../Components/Partner_Dashboard_Template/styles.css">
+    <link rel="stylesheet" href="./salesStyles.css">  
+    <link rel="stylesheet" href="./editCustomerStyles.css">     
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
 </head>
 <body>
@@ -142,13 +143,7 @@ if (isset($_GET['id'])) {
                         <input type="text" id="verificationStatus" name="verificationStatus" value="<?= htmlspecialchars($row['verificationStatus']); ?>" placeholder="Verification Status" required>
                     </div>
                     
-                    <div class="form-actions">
-                        <div class="form-actions">
-                            <button type="submit" class="btn-save">
-                                <i class='bx bx-save'></i> Save Changes
-                            </button>
-                        </div>
-                    </div>
+                    
                 </form>
             </div>
         </main>
@@ -158,7 +153,7 @@ if (isset($_GET['id'])) {
             document.querySelectorAll('input, select, textarea').forEach(input => input.disabled = true);
         </script>
         
-    <script src="../../../Components/Admin_Dashboard_Template/script.js"></script>
-    <script src="../../../Admin_Dashboard/script.js"></script>
+        <script src="../../../Components/Partner_Dashboard_Template/script.js"></script>
+        <script src="../../../Partners_Dashboard/script.js"></script>
 </body>
 </html>
